48716252d51782b16932b355437a7bca60b99d1d
MazDot <hamedmaz400@gmail.com>
2022-07-02 11:55:51 +0300

    Adding gRPC client for Basket API and tested

--- src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -7,7 +7,7 @@ namespace Basket.API.Entities
         public string Color { get; set; }
         public decimal Price { get; set; }
         public string ProductId { get; set; }
-        public string Productname { get; set; }
+        public string ProductName { get; set; }
 
 
     }
